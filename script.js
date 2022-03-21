1643a83ca296cbe19376b66efe399c4a6a05d7a5
erik schievenin <erik.schievenin@gmail.com>
2022-03-21 19:55:27 +0100

    Exercise soon

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
 console.log('JS OK!');
 
-Viene fornito un layout di base in cui è presente * una card di esempio * inserita staticamente nell’html.Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e * dovrà quindi essere rimossa dall’html *.
-Aggiungere un file js in cui definire * un array di oggetti * che rappresentano i membri del team.Ogni membro ha le informazioni necessarie per stampare la relativa card: * Nome, Ruolo e Foto *.
-Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
-BONUS:
-Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un *nuovo oggetto*, il quale viene *inserito nell'array iniziale * e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
-    * Consigli del giorno:*
-        Ragioniamo come sempre a step.
-Scriviamo prima la logica in italiano e poi traduciamo in codice.
-    console.log() è nostro amico.
-Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
+// Aggiungere un file js in cui definire *un array di oggetti* che rappresentano i membri del team. Ogni
+// membro ha le informazioni necessarie per stampare la relativa card: *Nome, Ruolo e Foto*.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html,
+// stampare dinamicamente una card per ogni membro del team.
+// BONUS:
+// Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team:
+// cliccando sul pulsante "add" viene creato un *nuovo oggetto*, il quale viene *inserito nell'array iniziale* e
+// viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+
+// Creazione *un array di oggetti* che rappresentano i membri del team.
+
+const characterPeople = [{
+    nome: 'Luca',
+    cognome: 'Rossi',
+    lavoro: 'Web Design'
+},
+{
+    nome: 'Mary',
+    cognome: 'Rossi',
+    lavoro: 'Web Design'
+},
+{
+    nome: 'Nadia',
+    cognome: 'Rossi',
+    lavoro: 'Web Design'
+},
+{
+    nome: 'Vittorio',
+    cognome: 'Rossi',
+    lavoro: 'Web Design'
+},
+{
+    nome: 'Eros',
+    cognome: 'Rossi',
+    lavoro: 'Web Design'
+},
+{
+    nome: 'Eros',
+    cognome: 'Rossi',
+    lavoro: 'Web Design'
+}];
+
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: *Nome, Ruolo e Foto*.
+
+for (let i = 0; i < characterPeople.length; i++) {
+    const detals = characterPeople[i];
+    console.log('ciclo una classe', detals);
+}
+
+// Stampare dinamicamente una card per ogni membro del team.
+let teamCard = document.querySelector('.team-card');
+
+for (let i = 0; i < characterPeople.length; i++) {
+
+    const worker = document.createElement('div');
+    worker.className = 'description';
+    teamCard.appendChild(worker);
+
+    worker.innerText = characterPeople.nome;
+}
